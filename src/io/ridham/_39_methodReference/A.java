153e4603aa8a54eca4686223230987735613227e
ridham35 <59654407+ridham35@users.noreply.github.com>
2020-07-05 12:27:41 +0530

    .gitignore

--- src/io/ridham/_39_methodReference/A.java
+++ src/io/ridham/_39_methodReference/A.java
@@ -2,11 +2,11 @@ package io.ridham._39_methodReference;
 
 // :: is used for method reference and constructor reference
 public class A {
-    // we can use lambada expression to give implementation to m1()
-    public static void main(String[] args) {
-        Interf i = () -> {
-            System.out.println("lambada implementation");
-        };
-        i.m1();
-    }
+	// we can use lambada expression to give implementation to m1()
+	public static void main(String[] args) {
+		Interf i = () -> {
+			System.out.println("lambada implementation");
+		};
+		i.m1();
+	}
 }
